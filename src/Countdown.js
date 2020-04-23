9808c6804b65d2089ff78302c98ba033d6db83fc
Nikita Work <bng@1sm.ru>
2020-04-23 09:32:09 +0500

    333

---
+++ src/Countdown.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+
+class Countdown extends React.Component {
+
+
+
+    render() {
+        return (
+            1
+        )
+
+    }
+
+
+}
+
+export default Countdown;
